133b7bf382f55fefe9d6331bfa024c869fd57ba8
Aluisio <aluisioordones1@gmail.com>
2022-07-07 16:19:10 -0300

    Completa requisitos 1 e 2.

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <h4
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h4>
+    );
+  }
+}
+
+export default Search;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,21 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const result = await fetch(endpoint);
+    const response = result.json();
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  try {
+    const end = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    const result = await fetch(end);
+    const response = result.json();
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
 }
