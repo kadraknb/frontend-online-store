4841d3ba84a796c6add15e6a4cca0f6d01e4d651
Aluisio <aluisioordones1@gmail.com>
2022-07-07 16:56:06 -0300

    Completa requisito 3.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
-//
+import ShoppingCart from './components/ShoppingCart';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path="/" component={ Search } />
+        <Switch>
+          <Route path="/shoppingCart" component={ ShoppingCart } />
+          <Route exact path="/" component={ Search } />
+        </Switch>
+        <Link to="/shoppingCart">
+          <button
+            type="button"
+            data-testid="shopping-cart-button"
+          >
+            Meu carrinho
+          </button>
+        </Link>
       </BrowserRouter>
     </div>
   );
